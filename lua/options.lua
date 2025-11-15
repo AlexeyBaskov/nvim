9d07f8ac7bf1e1708d29b7376325fba4f46d92e2
Aleksei <alex230105@gmail.com>
2025-11-15 16:20:03 +0100

    Initial commit

---
+++ lua/options.lua
@@ -0,0 +1,10 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.termguicolors = false
+vim.opt.list = true
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
