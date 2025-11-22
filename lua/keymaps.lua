4d47e6aae3c6eaf6f174751e86d66cd69b97ce8a
Aleksei <alex230105@gmail.com>
2025-11-23 00:31:16 +0100

    Formatting, completion, and LSP configuration for lua_ls and roslyn.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,2 +1,14 @@
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<leader>cd", function()
+	vim.diagnostic.jump({ count = 1, float = true })
+end)
+
+vim.keymap.set("n", "<leader>cD", function()
+	vim.diagnostic.jump({ count = -1, float = true })
+end)
+
+vim.keymap.set("n", "<leader>ca", function()
+	vim.lsp.buf.code_action()
+end, { desc = "Code actions" })
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,20 +6,110 @@ vim.pack.add({
 	git_appendix .. "mason-org/mason.nvim",
 	git_appendix .. "mason-org/mason-lspconfig.nvim",
 	git_appendix .. "stevearc/conform.nvim",
+	git_appendix .. "saghen/blink.cmp",
+	git_appendix .. "seblyng/roslyn.nvim",
 })
 
 local wk = require("which-key")
 
-require("mason").setup()
-require("mason-lspconfig").setup({
-	ensure_installed = { "lua_ls", "stylua" },
+require("mason").setup({
+	registries = {
+		"github:mason-org/mason-registry",
+		"github:Crashdummyy/mason-registry",
+	},
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
 })
+
+require("mason-lspconfig").setup({
+	ensure_installed = {
+		"lua_ls",
+		"stylua",
+		-- "roslyn",
+		-- "csharpier",
+	},
+})
+
 require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
+		cs = { "csharpier" },
 	},
 	format_on_save = {
 		timeout_ms = 500,
 		lsp_format = "fallback",
 	},
+})
+
+vim.lsp.config("lua_ls", {
+	settings = {
+		Lua = {
+			runtime = {
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
+})
+
+require("roslyn").setup()
+
+vim.lsp.config("roslyn", {
+	settings = {
+		["csharp|background_analysis"] = {
+			dotnet_analyzer_diagnostics_scope = "fullSolution",
+			dotnet_compiler_diagnostics_scope = "fullSolution",
+		},
+		["csharp|code_lens"] = {
+			dotnet_enable_references_code_lens = true,
+			dotnet_enable_tests_code_lense = true,
+		},
+		["csharp|completion"] = {
+			dotnet_provide_regex_completions = true,
+			dotnet_show_completion_items_from_unimported_namespaces = true,
+			dotnet_show_name_completion_suggestions = true,
+		},
+		["csharp|inlay_hints"] = {
+			csharp_enable_inlay_hints_for_implicit_object_creation = true,
+			csharp_enable_inlay_hints_for_implicit_variable_types = true,
+			csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+			csharp_enable_inlay_hints_for_types = true,
+			dotnet_enable_inlay_hints_for_indexer_parameters = true,
+			dotnet_enable_inlay_hints_for_literal_parameters = true,
+			dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+			dotnet_enable_inlay_hints_for_other_parameters = true,
+			dotnet_enable_inlay_hints_for_parameters = true,
+			dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+			dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+			dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+		},
+		["csharp|symbol_search"] = {
+			dotnet_search_reference_assemblies = true,
+		},
+	},
+})
+
+require("blink.cmp").setup({
+	keymap = {
+		preset = "enter",
+	},
+})
+
+vim.diagnostic.config({
+	--underline = true,
+	--virtual_text = true,
+	virtual_lines = true,
 })
